3aba8603083f20820e5ec5295adfa71eaae353ff
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-09-15 14:00:40 +0300

    category page

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,7 +1,34 @@
-export default function ProductPage({ params }) {
+import CategoryNav from '@/components/CategoryNav'
+import Product from '@/components/Product'
+import { getData } from '@/lib/data'
+
+export default async function ProductPage({ params }) {
+	const id = params.id
+	const { data } = await getData(
+		`/products?populate=*&filters[categories][id][$eq]=${id}`
+	)
+
 	return (
-		<main className='text-7xl font-bold underline container h-screen w-screen flex  items-center justify-center'>
-			Products Page {params.id}
-		</main>
+		<div className='mb-16 pt-40 lg:pt-0 '>
+			<div className='container'>
+				<div className='flex gap-x-10'>
+					<div className='lg:w-2/12 hidden lg:block'>
+						<CategoryNav />
+					</div>
+					<main className='lg:w-10/12 w-full'>
+						{/* title */}
+						<h1 className='uppercase pb-4 font-bold text-2xl'>
+							{data[0]?.attributes.categories.data[0].attributes.title} cameras
+						</h1>
+						{/* product grid */}
+						<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-10'>
+							{data?.map((product) => {
+								return <Product product={product} key={product.id} />
+							})}
+						</div>
+					</main>
+				</div>
+			</div>
+		</div>
 	)
 }
